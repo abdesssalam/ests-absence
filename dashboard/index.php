5a78e689bd2c1802560f21e0ca2d165d77c4dbff
oussama <oussama.agnina000@gmail.com>
2022-12-18 21:42:59 +0100

    test affichageb bien

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,3 +1,22 @@
 <?php require_once '../includes/header.php' ?>
 dashboard
+<?php
+$persons = simplexml_load_file('../db/absence.xml');
+
+?>
+<table>
+	<tr>
+		<th>nom</th>
+		<th>prenom</th>
+
+	</tr>
+	<?php foreach($persons->person as $person) { ?>
+	<tr>
+		<td><?php echo $person->nom; ?></td>
+		<td><?php echo $person->prenom; ?></td>
+
+    </tr>
+    <?php
+    }
+    ?>
 <?php require_once '../includes/footer.php' ?>
