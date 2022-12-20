ad543ac074366ab34700edd35c9725d21ba575ce
oussama <oussama.agnina000@gmail.com>
2022-12-20 13:16:48 +0100

    first&last_nameOf_A_Role_varies

--- content/contacts/contact.php
+++ content/contacts/contact.php
@@ -17,7 +17,7 @@
         <tbody>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                   Abdessalam ait omar
+                <input type="text" id="fullname" class="border border-black text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nom & prenom" required>
                 </th>
                 <td class="py-4 px-6">
                     <select >
